828fc809695d231feee73e9c9ee0012633744b0e
Julius Angwenyi <julius.bichage@gmail.com>
2016-04-06 13:06:48 +0100

    New projects to process d2c messages into azure storage and service bus

---
+++ ProofOfConceptNesh/ProcessD2CInteractiveMessages/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.ServiceBus.Messaging;
+
+
+namespace ProcessD2CInteractiveMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Process D2C Interactive Messages app\n");
+
+            string connectionString = "{service bus listen connection string}";
+            QueueClient Client = QueueClient.CreateFromConnectionString(connectionString, "d2cnesh");
+
+            OnMessageOptions options = new OnMessageOptions();
+            options.AutoComplete = false;
+            options.AutoRenewTimeout = TimeSpan.FromMinutes(1);
+
+            Client.OnMessage((message) =>
+            {
+                try
+                {
+                    var bodyStream = message.GetBody<Stream>();
+                    bodyStream.Position = 0;
+                    var bodyAsString = new StreamReader(bodyStream, Encoding.ASCII).ReadToEnd();
+
+                    Console.WriteLine("Received message: {0} messageId: {1}", bodyAsString, message.MessageId);
+
+                    message.Complete();
+                }
+                catch (Exception)
+                {
+                    message.Abandon();
+                }
+            }, options);
+
+            Console.WriteLine("Receiving interactive messages from SB queue...");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ProofOfConceptNesh/ProcessDeviceToCloudMessages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ProcessDeviceToCloudMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iotHubConnectionString = "{iot hub connection string}";
+            string iotHubD2cEndpoint = "messages/events";
+            StoreEventProcessor.StorageConnectionString = "{storage connection string}";
+            StoreEventProcessor.ServiceBusConnectionString = "{service bus send connection string}";
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, iotHubD2cEndpoint, EventHubConsumerGroup.DefaultGroupName, iotHubConnectionString, StoreEventProcessor.StorageConnectionString, "messages-events");
+            Console.WriteLine("Registering EventProcessor...");
+            eventProcessorHost.RegisterEventProcessorAsync<StoreEventProcessor>().Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
